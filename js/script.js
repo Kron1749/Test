b4d67fc4c2118b7f00e078031dd63c2e4fbf5bcc
Kron <drakgr98@gmail.com>
2022-02-10 15:19:41 +0300

    Task3

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
 
-let numberOfFilms = +prompt("Cколько фильмов смотрели?", '');
+
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Cколько фильмов смотрели?", '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Cколько фильмов смотрели?", '');
+    }
+}
+
+start();
 
 
 const personalMovieDB =  {
@@ -14,25 +26,47 @@ const personalMovieDB =  {
 
 let a;
 let b;
-for(let i =0;i<2;i++) {
-    a = prompt("Последний фильм?");
-    b = prompt("Оцените этот фильм");
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
+
+function showMyDB(hidden){
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } 
+}
+
+function writeYourGenres() {
+    for (let i =0;i<=2;i++) {
+        personalMovieDB.genres[i] = prompt(`Ваш любимы жанр под номером ${i}`);
     }
 }
 
-if (personalMovieDB.count < 10) {
-    
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Просмотрено нормально фильмов");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Просмотрено много фильмов");
-} else {
-    console.log("Ошибка");
+function rememberMyFilms() {
+    for(let i =0;i<2;i++) {
+        a = prompt("Последний фильм?");
+        b = prompt("Оцените этот фильм");
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
 }
 
+rememberMyFilms();
+
+
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+    
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Просмотрено нормально фильмов");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Просмотрено много фильмов");
+    } else {
+        console.log("Ошибка");
+    }
+}
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
